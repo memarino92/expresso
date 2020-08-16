f26a3e27c5b4fcf02f9f25778db177681d8560bc
Mike <62777339+memarino92@users.noreply.github.com>
2020-08-16 14:58:15 -0400

    employee routes passing tests

---
+++ api/employees.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+
+const employeesRouter = express.Router();
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+employeesRouter.param('employeeId', (req, res, next, employeeId) => {
+    db.get(`SELECT * FROM Employee WHERE id = ${employeeId}`, (err, row) => {
+        if (err) {
+            next(err);
+        } else if (row) {
+            req.employee = row;
+            next();
+        } else {
+            res.status(404).send();
+        }
+    });
+});
+
+employeesRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM Employee WHERE is_current_employee = 1`, (err, rows) => {
+        if (err) {
+            next(err);
+        } else {
+            res.send({employees: rows});
+        }
+    });
+});
+
+employeesRouter.post('/', (req, res, next) => {
+    const employee = req.body.employee;
+    if (employee.name &&
+        employee.position &&
+        employee.wage) {
+            if (!employee.isCurrentEmployee) {
+                employee.isCurrentEmployee = 1;
+            }
+            const { name, position, wage, isCurrentEmployee } = employee;
+            db.run(`INSERT INTO Employee (
+                name,
+                position,
+                wage,
+                is_current_employee
+            ) VALUES (
+                '${name}',
+                '${position}',
+                ${wage},
+                ${isCurrentEmployee}
+            )`, function(err) {
+                if (err) {
+                    next(err);
+                } else {
+                    db.get(`SELECT * FROM Employee WHERE id = ${this.lastID}`, (err, row) => {
+                        if (err) {
+                            next(err);
+                        } else {
+                            res.status(201).send({employee: row});
+                        }
+                    })
+                }
+            });
+        } else {
+            res.status(400).send();
+        }
+});
+
+employeesRouter.get('/:employeeId', (req, res, next) => {
+    res.send({employee: req.employee});
+});
+
+employeesRouter.put('/:employeeId', (req, res, next) => {
+    const employee = req.body.employee;
+    if (employee.name &&
+        employee.position &&
+        employee.wage) {
+            if (!employee.isCurrentEmployee) {
+                employee.isCurrentEmployee = 1;
+            }
+            const { name, position, wage, isCurrentEmployee } = employee;
+            db.run(`UPDATE Employee SET
+            name = '${name}',
+            position = '${position}',
+            wage = ${wage},
+            is_current_employee = ${isCurrentEmployee}
+            WHERE id = ${req.params.employeeId}`, function(err) {
+                if (err) {
+                    next(err);
+                } else {
+                    db.get(`SELECT * FROM Employee WHERE id = ${req.params.employeeId}`, (err, row) => {
+                        if (err) {
+                            next(err);
+                        } else {
+                            res.send({employee: row});
+                        }
+                    });
+                }
+            });
+        } else {
+            res.status(400).send();
+        }
+});
+
+employeesRouter.delete('/:employeeId', (req, res, next) => {
+    db.run(`UPDATE Employee
+    SET is_current_employee = 0
+    WHERE id = ${req.params.employeeId}`, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            db.get(`SELECT * FROM Employee WHERE id = ${req.params.employeeId}`, (err, row) => {
+                if (err) {
+                    next(err);
+                } else {
+                    res.send({employee: row});
+                }
+            });
+        }
+    });
+});
+
+module.exports = employeesRouter;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const errorhandler = require('errorhandler');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const apiRouter = require('./api/api');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,10 +12,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-
+app.use('/api', apiRouter);
 
 app.use(errorhandler())
 
 app.listen(PORT, () => {
     console.log("Go ahead caller, I'm Listening...")
 });
+
+module.exports = app;
